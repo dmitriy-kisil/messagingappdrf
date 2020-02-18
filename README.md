ecc48042a2108bec9fbd671256aa393a5e85fc15
dmitriy <logart1995@gmail.com>
2020-02-18 17:15:32 +0200

    Initialize the project

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple messaging app using DRF
---
+++ chat/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
+    message = models.CharField(max_length=1200)
+    subject = models.CharField(max_length=200)
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
+
+    class Meta:
+        ordering = ('creation_date',)
