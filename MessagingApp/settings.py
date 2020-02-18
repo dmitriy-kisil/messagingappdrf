f132f2a8cb14f90e59ec6f554b2c475d3f12f4f0
dmitriy <logart1995@gmail.com>
2020-02-18 17:28:34 +0200

    Add staticmediaurls

--- MessagingApp/settings.py
+++ MessagingApp/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# location where django collect all static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # location where you will store your static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'MessagingApp/static')]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
